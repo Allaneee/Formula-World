9e0be6ecbfecf184ba1eb2fa385460ef0684cd19
Francois.Garnier <francois.garnier@masselin.fr>
2024-03-21 05:39:27 +0100

    logo team

--- app/src/main/java/Adapter/TeamRankAdapter.java
+++ app/src/main/java/Adapter/TeamRankAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -52,38 +53,49 @@ public class TeamRankAdapter extends RecyclerView.Adapter<TeamRankAdapter.TeamVi
 
         switch (currentTeam.getname()) {
             case "Ferrari":
+                holder.imgLogo.setImageResource(R.drawable.ferrari_logo);
                 holder.itemView.setBackgroundResource(R.drawable.ferrari);
                 break;
             case "Red Bull":
+                holder.imgLogo.setImageResource(R.drawable.rb_logo);
                 holder.itemView.setBackgroundResource(R.drawable.red_bull);
                 break;
             case "McLaren":
+                holder.imgLogo.setImageResource(R.drawable.mclaren_logo);
                 holder.itemView.setBackgroundResource(R.drawable.mclaren);
                 break;
             case "Mercedes":
+                holder.imgLogo.setImageResource(R.drawable.mercedes_logo);
                 holder.itemView.setBackgroundResource(R.drawable.mercedes);
                 break;
             case "Aston Martin":
+                holder.imgLogo.setImageResource(R.drawable.aston_logo);
                 holder.itemView.setBackgroundResource(R.drawable.aston_martin);
                 break;
             case "Williams":
+                holder.imgLogo.setImageResource(R.drawable.williams_logo);
                 holder.itemView.setBackgroundResource(R.drawable.williams);
                 break;
             case "Sauber":
+                holder.imgLogo.setImageResource(R.drawable.stake_logo);
                 holder.itemView.setBackgroundResource(R.drawable.stake);
                 break;
             case "Alpine F1 Team":
+                holder.imgLogo.setImageResource(R.drawable.alpine_logo);
                 holder.itemView.setBackgroundResource(R.drawable.alpine);
                 break;
             case "Haas F1 Team":
+                holder.imgLogo.setImageResource(R.drawable.haas_logo);
                 holder.itemView.setBackgroundResource(R.drawable.haas);
                 break;
             case "RB F1 Team":
+                holder.imgLogo.setImageResource(R.drawable.bulls_logo );
                 holder.itemView.setBackgroundResource(R.drawable.racing_bulls);
                 break;
 
             default:
-                holder.itemView.setBackgroundResource(R.drawable.verstappen);
+                holder.imgLogo.setImageResource(R.drawable.ferrari_logo);
+                holder.itemView.setBackgroundResource(R.drawable.aston_martin);
                 break;
         }
     }
@@ -99,11 +111,14 @@ public class TeamRankAdapter extends RecyclerView.Adapter<TeamRankAdapter.TeamVi
         TextView tvPoints;
         TextView tvRanking;
 
+        ImageView imgLogo;
+
         TeamViewHolder(View itemView) {
             super(itemView);
             tvName = itemView.findViewById(R.id.tvTeamName);
             tvPoints = itemView.findViewById(R.id.tvTeamPoints);
             tvRanking = itemView.findViewById(R.id.tvTeamRanking);
+            imgLogo = itemView.findViewById(R.id.imgLogo);
         }
     }
     @SuppressLint("NotifyDataSetChanged")
